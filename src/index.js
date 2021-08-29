af868ea12bd1e7b1e1264c442bce259646d4a3c3
AlexandrePeres97 <alexandrelp97@hotmail.com>
2021-08-29 15:41:47 -0300

    Atualização do código com STATUS CODE

--- src/index.js
+++ src/index.js
@@ -10,17 +10,29 @@ let clients = [
     { id: 4, nome: 'Angelo Escuro', telefone: '53981369699' },
 ]
 
+function log (request, response, next){
+    const {url, method} = request;
+    console.log(`${method} - ${url} at ${new Date()}`)
+    return next();
+}
+app.use(log)
 /**
  * Retorna todos clientes em json
  */
-app.get('/clients', (request, response) => response.json(clients))
+ app.get('/clients', (request, response) => response.status(200).json(clients))
 
-/**
+ /**
  * Buscar UM único recurso
  */
 app.get('/clients/:id', (request, response) => {
-    const client = clients.filter(value => value.id == request.params.id);
-    response.json(client);
+    const {id} = request.params;
+    const client = clients.find(value => value.id == id);
+    if(client == undefined){
+        response.status(400).send();
+    }else{
+        response.status(200).json({error: 'Requisição inválida'});
+    }
+    //response.json(clients.filter(value => value.id == request.params.id)))
 })
 
 /**
@@ -29,29 +41,39 @@ app.get('/clients/:id', (request, response) => {
 app.post('/clients', (request, response) =>{
     const client = request.body;
     clients.push(client);
-    response.json(client);
+    response.status(201).json(client);
 
 })
 
 /**
  * Atualizar nome de clientes
  */
- app.put('/clients/:id', (request, response) => {
+app.put('/clients/:id', (request, response) => {
     const id = request.params.id;
     const nome = request.body.nome;
-    let client = clients.filter(value => value.id == id);
-    client[0].nome = nome;
-    response.json(client[0]);
+    let client = clients.find(value => value.id == id);
+    if(client == undefined){
+        response.status(400).send();
+    }else{
+        client.nome = nome;
+        response.status(200).json(client);
+    }
 
 })
 
 /**
  * Exclui o cliente atravez do id
  */
-app.delete('/clients/:id', (request, response) => {
-    const id = request.params.id;
-    clients = clients.filter(value => value.id != id);
-    response.json(clients);
+ app.delete('/clients/:id', (request, response) => {
+    const {id} = request.params;
+    const index = clients.findIndex(value => value.id == id);
+    if(index == -1){
+        response.status(400).send();
+    }else{
+        clients.splice(index,1);
+        response.status(204).send();
+    }
+    
 
 })
 
